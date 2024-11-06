dbb836437e78ff1a7668a1104e7922f90d3e0b94
Prince Nishad <pn3147399@gmail.com>
2024-11-06 12:07:32 +0530

    change base url into .env file

--- src/Redux/querySlice.js
+++ src/Redux/querySlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5000",
+        baseUrl: import.meta.env.VITE_BASE_API_URL,
             prepareHeaders: (headers, { getState }) => {
       // Get the token from the state or storage
       const token = getState().auth.token || localStorage.getItem('token');
